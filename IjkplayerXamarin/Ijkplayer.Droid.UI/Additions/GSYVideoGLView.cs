b7feeec6211db0ddc7c1a65d148ef426bf89e556
json <123@qqcom>
2019-04-18 00:03:11 +0800

    UI

---
+++ IjkplayerXamarin/Ijkplayer.Droid.UI/Additions/GSYVideoGLView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Shuyu.Gsyvideoplayer.Render.View
+{
+    public partial class GSYVideoGLView : global::Android.Opengl.GLSurfaceView
+    {
+
+        public void SetRenderMode(int p0)
+        {
+            this.RenderMode = (Android.Opengl.Rendermode)p0;
+        }
+    }
+}
